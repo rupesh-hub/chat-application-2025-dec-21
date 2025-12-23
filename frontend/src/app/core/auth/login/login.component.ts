a2b811803f6e8ba8fdfaf7dbcc83c1fc9dafe602
rupesh672 <=>
2025-12-23 22:43:38 +0545

    feat: frontend update

--- frontend/src/app/core/auth/login/login.component.ts
+++ frontend/src/app/core/auth/login/login.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'chat-login',
   imports: [],
+  standalone: true,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
